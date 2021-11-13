ab7d4fdc26534be00e9ef0bb097b5354986d8067
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-11-13 19:34:45 +0530

    Add files via upload

---
+++ ifElseQuickQuiz.py
@@ -0,0 +1,7 @@
+# Write a program to print yes when the age entered by the user is greater than or equal to 18.
+# age=18
+age=float(input("Enter Your Age: "))
+if age>=18:
+    print("Yes")
+else:
+    print("No")
